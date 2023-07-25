36322882bb08e3c121d184605b4593f7f79eb570
eden <99998327+edenosman@users.noreply.github.com>
2023-07-25 18:56:45 +0300

    Console Application Assignment

---
+++ ConsoleApplicationAssignment/ConsoleApplicationAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApplicationAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int Salary = 2000;
+            //Console.WriteLine(Salary * 50);
+            //Console.ReadLine();
+
+            //float num1 = 25.5f;
+            //Console.WriteLine(num1 + 25);
+            //Console.ReadLine();
+
+            //int num2 = 100;
+            //Console.WriteLine(num2 / 12.5);
+            //Console.ReadLine();
+
+            //int num3 = 60;
+            //bool greaterThan50 = num3 > 50;
+            //Console.WriteLine(greaterThan50);
+            //Console.ReadLine();
+
+            //int remainder = 50;
+            //Console.WriteLine(remainder % 7);
+            //Console.ReadLine();
+        }
+    }
+}
