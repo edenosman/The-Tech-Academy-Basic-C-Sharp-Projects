a93e3b844bf6d0794ca6b7ae00a590a7bccafe46
eden <99998327+edenosman@users.noreply.github.com>
2023-10-05 17:16:51 +0300

    Method Assignment

---
+++ Method Assignment/Method Assignment/MathOperation1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Method_Assignment
+{
+    class MathOperation1
+    {
+        // Method that takes two integers as parameters
+        // Make one parameter optional (provides a default value)
+        public int MathOperation(int a, int b = 1)
+        {
+            // Perform multiplication on integers and return the result
+            int result = a * b;
+            return result;
+        }
+    }
+}
---
+++ Method Assignment/Method Assignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate MathOperation1 class
+            MathOperation1 mathOp = new MathOperation1();
+            // Ask the user to enter first parameter
+            Console.WriteLine("Please enter an integer: ");
+            int nr1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user to enter a second parameter or press enter
+            Console.WriteLine("Please enter a second parameter or just press enter: ");
+            try
+            {
+                // Call method with 2 parameters if both parameters are provided
+                int nr2 = Convert.ToInt32(Console.ReadLine());
+                int results = mathOp.MathOperation(nr1, nr2);
+                Console.WriteLine(nr1 + " * " + nr2 + " = " + results);
+            }
+            catch
+            {
+                // Call method with one parameter if only one is provided
+                int results = mathOp.MathOperation(nr1);
+                Console.WriteLine(nr1 + " * 1 = " + results);
+            }
+            Console.ReadLine();
+        }
+    }
+}
