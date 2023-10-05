d11c8c965e89b0fafad0a2dd9397f6f251b3bc94
eden <99998327+edenosman@users.noreply.github.com>
2023-10-05 16:58:49 +0300

    Add files via upload

---
+++ Main Method Assignment/Main Method Assignment/MainMethods2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Main_Method_Assignment
+{
+    public class MainMethods2
+    {
+        // First method - takes an integer
+        public int MathOp(int nr)
+        {
+            return nr + 10;
+        }
+        // Second method - takes a decimal
+        public int MathOp(decimal nr)
+        {
+            // Convert the decimal to an int to perform math operation
+            int newnr = Convert.ToInt32(nr);
+            return newnr * 15;
+        }
+        // Third method - takes a string
+        public int MathOp(string nr)
+        {
+            // Convert the string to an int to perform math operation
+            int newnr = Convert.ToInt32(nr);
+            return newnr % 5;
+        }
+    }
+}
---
+++ Main Method Assignment/Main Method Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Main_Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MainMethods2 - class
+            MainMethods2 math1 = new MainMethods2();
+            // Call the first method with an int parameter
+            Console.WriteLine(math1.MathOp(10));
+            // Call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(10.5m));
+            // Call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("10"));
+        }
+    }
+}
