59dac8f52f89db1434d12eec3b0b3f17490369ce
eden <99998327+edenosman@users.noreply.github.com>
2023-10-06 16:30:12 +0300

    Polymorphism Assignment

---
+++ Polymorphism Assignment/Polymorphism Assignment/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Polymorphism_Assignment
+{
+    // Define an interface 
+    public interface IQuittable
+    {
+        // Define a void method
+        void Quit();
+    }
+    
+    // Define a class that implements the interface created above
+    public class Employee : IQuittable
+    {
+        // Implement the Quit method from the interface
+        public void Quit()
+        {
+            // Print a message to the console
+            Console.WriteLine("The employee has quit.");
+        }
+    }
+}
---
+++ Polymorphism Assignment/Polymorphism Assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Polymorphism_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use polymorphism to create an object of type IQuittable
+            IQuittable quittable = new Employee();
+            // Call the Quit method on the IQuittable object
+            quittable.Quit();
+        }
+    }
+}
