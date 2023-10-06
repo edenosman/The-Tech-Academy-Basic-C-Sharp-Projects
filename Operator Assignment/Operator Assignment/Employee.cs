1c01324532c5293f9b3307632dc95f4499ddf5fd
eden <99998327+edenosman@users.noreply.github.com>
2023-10-06 16:48:51 +0300

    Operator Assignment

---
+++ Operator Assignment/Operator Assignment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Operator_Assignment
+{
+    // Create a class - Employee
+    public class Employee
+    {
+        // Define properties for Id, FirstName, and LastName
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Overload the '==' operator
+        public static bool operator ==(Employee e1, Employee e2)
+        {
+            // check if two Employee objects are equal by comparing their Id
+            return e1.Id == e2.Id;
+        }
+
+        // Overload the '!=' operator
+        public static bool operator !=(Employee e1, Employee e2)
+        {          
+            // Check if two Employee objects are not equal by comparing their Id
+            return e1.Id != e2.Id;
+        }
+    }
+}
---
+++ Operator Assignment/Operator Assignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Operator_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create two Employee objects with the same Id
+            Employee employee1 = new Employee { Id = 1, FirstName = "Eden", LastName = "Osman" };
+            Employee employee2 = new Employee { Id = 1, FirstName = "Johnny", LastName = "Bravo" };
+
+            // Use the overloaded '==' operator to check if the two employees are equal
+            Console.WriteLine(employee1 == employee2);
+
+            // Create another Employee object with a different Id
+            Employee employee3 = new Employee { Id = 2, FirstName = "Joe", LastName = "Cena" };
+
+            // Use the overloaded '!=' operator to check if two employees are equal
+            Console.WriteLine(employee1 != employee3);
+        }
+    }
+}
