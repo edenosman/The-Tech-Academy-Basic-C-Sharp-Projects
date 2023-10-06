ca208e2ab031c31e455bdcf6e9833040b576fe72
eden <99998327+edenosman@users.noreply.github.com>
2023-10-07 00:40:10 +0300

    Entity Framework Code-First Assignment

---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new School_Context())
+            {
+                // Create a new student object
+                var stud = new Student() { StudentName = "Eden" };
+
+                // Add the new student to the students DbSet
+                ctx.Students.Add(stud);
+
+                // Save the changes to the database
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EntityFrameworkCodeFirst/EntityFrameworkCodeFirst/School Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityFrameworkCodeFirst
+{
+    class School_Context: DbContext 
+    {
+        public School_Context(): base()
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
