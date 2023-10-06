d26c621c1e42a218ada1a93034af1ae31162656d
eden <99998327+edenosman@users.noreply.github.com>
2023-10-06 16:21:28 +0300

    Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MethodClassAssignment
+{
+    // Create a new class
+    public class MyClass
+    {
+        // Create a void method (MyMethod) that takes 2 integers as parameters
+        public void MyMethod(int num1, int num2)
+        {
+            // Perform a math op on the first int
+            num1 += 1;
+            // Display the second int to the screen
+            Console.WriteLine("The second number is " + num2);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MyClass
+            MyClass myClassInstance = new MyClass();
+            // Call the MyMethod in the MyClass, passing in two numbers
+            myClassInstance.MyMethod(5, 10);
+            // Call the MyMethod in the MyClass, specifying the parameters by name
+            myClassInstance.MyMethod(num1: 20, num2: 30);
+        }
+    }
+}
